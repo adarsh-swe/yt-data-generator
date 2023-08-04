f17c1831316c6c89c6424e942a41dbbe34146d3e
Adarsh Patel <swe.adarsh@gmail.com>
2023-08-04 00:02:48 -0400

    add image and textgeneration

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("testing");
+const readline = require("readline");
+const {
+	generateMeta,
+	generateImage,
+} = require("./controllers/openaiController");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+// rl.question("youtube vid title: \n", generateMeta);
+rl.question("describe your youtube thumbnail: \n", generateImage);
